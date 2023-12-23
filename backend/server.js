82453df7e468783c116bc2b58dcb9cfbea4b9e0d
sagar-7227 <sagarvashnav72427@gmail.com>
2023-12-23 20:59:53 +0530

    left files

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
     pingTimeout: 60000, // close connection if ping is not received within 60 seconds
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-app-jxt7.onrender.com",
         // credentials: true,
     },
 });
